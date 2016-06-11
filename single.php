4ae1b3ffe2a106b711907239591b9f38aa27be7f
mkkagain <kristian@stoeckel-mail.de>
2016-06-11 18:24:40 +0200

    +Autor Box

--- single.php
+++ single.php
@@ -9,10 +9,27 @@
                     <div class="news">
                         <h3><?php the_title(); ?></h3>
                         <span class="author" style="display: none;">Geschrieben von <?php the_author(); ?> am <?php the_date("d.m.Y"); ?></span>
-                        <!-- TODO: Featured Image -->
                         <article><?php the_content(); ?></article>
                         <div class="clear"></div>
-                        <hr class="newsDivider" />
+
+                        <div style="border: #004287 solid 1px; background-color: #e7e7e7; padding: 15px; margin-bottom: 30px;" id="authorBox">
+                            <?php echo get_avatar(get_the_author_meta('ID'), 100, NULL, 'Profilbild von ' . get_the_author()); ?>
+                            <table>
+                                <tr>
+                                    <td style="font-weight: bold;">Geschrieben von: </td>
+                                    <td><?php the_author(); ?></td>
+                                </tr>
+                                <tr <?php echo (get_the_author_meta('funktionStamm') == '' ? 'style="display: none;"' : ''); ?>>
+                                    <td style="font-weight: bold;">Funktion im Stamm: </td>
+                                    <td><?php the_author_meta('funktionStamm'); ?></td>
+                                </tr>
+                                <tr <?php echo (get_the_author_meta('pfadiSince') == '' ? 'style="display: none;"' : ''); ?>>
+                                    <td style="font-weight: bold;">Pfadfinder seit: </td>
+                                    <td><?php the_author_meta('pfadiSince'); ?></td>
+                                </tr>
+                            </table>
+                            <div class="clear"></div>
+                        </div>
                     </div>
                     <?php
                 endwhile;
