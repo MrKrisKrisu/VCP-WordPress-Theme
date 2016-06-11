e1664105ecee5a0900cd26ebaa832fd6b522ccb1
mkkagain <kristian@stoeckel-mail.de>
2016-06-12 00:07:38 +0200

    +Strukturierte Daten

--- single.php
+++ single.php
@@ -6,18 +6,25 @@
             if (have_posts()) {
                 while (have_posts()) : the_post();
                     ?>
-                    <div class="news">
-                        <h3><?php the_title(); ?></h3>
+                    <div class="news" itemscope itemtype="http://schema.org/NewsArticle">
+                        <meta itemprop="datePublished" content="<?php echo get_the_date("Y-m-d") . "T" . get_the_date("H:i:s"); ?>+02:00" />
+                        <meta itemprop="dateModified" content="<?php echo get_the_modified_date("Y-m-d") . "T" . get_the_modified_date("H:i:s"); ?>+02:00" />
+                        <meta itemprop="publisher" content="VCP St. Georg" />
+                        <h3 itemprop="headline"><?php the_title(); ?></h3>
                         <span class="author" style="display: none;">Geschrieben von <?php the_author(); ?> am <?php the_date("d.m.Y"); ?></span>
-                        <article><?php the_content(); ?></article>
+                        <article itemprop="articleBody"><?php the_content(); ?></article>
                         <div class="clear"></div>
 
                         <div style="border: #004287 solid 1px; background-color: #e7e7e7; padding: 15px; margin-bottom: 30px;" id="authorBox">
                             <?php echo get_avatar(get_the_author_meta('ID'), 100, NULL, 'Profilbild von ' . get_the_author()); ?>
                             <table>
                                 <tr>
+                                    <td style="font-weight: bold;">Geschrieben am: </td>
+                                    <td><time itemprop="datePublished" datetime="<?php the_date("Y-m-d"); ?>"><?php the_date(); ?></time></td>
+                                </tr>
+                                <tr>
                                     <td style="font-weight: bold;">Geschrieben von: </td>
-                                    <td><?php the_author(); ?></td>
+                                    <td itemprop="author" itemtype="http://schema.org/Person"><?php the_author(); ?></td>
                                 </tr>
                                 <tr <?php echo (get_the_author_meta('funktionStamm') == '' ? 'style="display: none;"' : ''); ?>>
                                     <td style="font-weight: bold;">Funktion im Stamm: </td>
