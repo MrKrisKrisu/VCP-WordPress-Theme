efae92e48338fc6133917acdd9bdfca127c6078a
MrKrisKrisu <kristian@stoeckel-mail.de>
2016-07-03 11:46:36 +0200

    Optimierung für Mobilgeräte

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="de">
     <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
         <title><?php wp_title(); ?> - <?php bloginfo('name'); ?></title>
